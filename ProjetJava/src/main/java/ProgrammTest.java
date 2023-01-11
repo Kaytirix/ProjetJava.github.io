325c2f4b0d489349f4c39778113bd45904040d7b
remi2001 <ometzremi54@gmail.com>
2023-01-11 11:38:56 +0100

    Depot 1
    Depot 1

--- ProjetJava/src/main/java/ProgrammTest.java
+++ ProjetJava/src/main/java/ProgrammTest.java
@@ -1,5 +1,31 @@
+import java.io.Console;
+import java.io.IOException;
+
 public class ProgrammTest {
     public static void main(String[] args) {
-        System.out.println("Hello World !");
+
+        String rep= "Oui";
+
+        while (rep=="Oui") {
+
+            System.out.println("Voulez-vous ajouter un livre ? (Oui/Non)");
+
+            try {
+                rep = //Saisie Clavier
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println(rep);
+            /*
+
+            if (rep == "Oui") {
+                System.out.println("Saisir un Livre a ajouter : ");
+                try {
+                    System.in.read();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }*/
+        }
     }
 }
